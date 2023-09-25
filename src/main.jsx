10550e44ca796c99b37ba838f9b2d3730642cbb7
OneTooMany <santosismael.115@gmail.com>
2023-09-25 14:20:50 -0700

    Mv main.jsx outside of components for proper render

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+import App from "./components/App.jsx";
 import "./styles/index.css";
+import CVForm from "./components/CVForm.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
