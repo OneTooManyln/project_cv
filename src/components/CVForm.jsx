60a9b8bb1ccf5ea7909f576188afb84cbf921608
OneTooMany <santosismael.115@gmail.com>
2023-09-27 16:27:14 -0700

    Expanded education & experience section
    Included content for preview-info's
    - Add dummy text to get basic visual layout correct

--- src/components/CVForm.jsx
+++ src/components/CVForm.jsx
@@ -15,13 +15,37 @@ export default function CVForm() {
             <div className="education-section-header">
               <h2>Education</h2>
             </div>
-            <div className="education-section-preview-info"></div>
+            <div className="education-section-preview-info">
+              <div className="preview-info-date">
+                <p>1974-1978</p>
+                <p>New Haven, Connecticut</p>
+              </div>
+              <div className="preview-info-degree">
+                <p>
+                  {" "}
+                  <strong>Yale university at Gotham</strong>{" "}
+                </p>
+                <p>Law Degree</p>
+              </div>
+            </div>
           </div>
           <div className="CVForm-main-experience-section">
             <div className="experience-section-header">
               <h2>Work Experience</h2>
             </div>
-            <div className="experience-section-preview-info"></div>
+            <div className="experience-section-preview-info">
+              <div className="preview-info-date">
+                <p>???-???</p>
+                <p>Gotham City, US</p>
+              </div>
+              <div className="preview-info-degree">
+                <p>
+                  {" "}
+                  <strong>Wayne Enterprises, Inc.</strong>{" "}
+                </p>
+                <p>CEO</p>
+              </div>
+            </div>
           </div>
         </div>
       </div>
