208dffcf0ec6d88e90a7548d15b3dc5da24fc084
OneTooMany <santosismael.115@gmail.com>
2023-09-27 17:32:23 -0700

    Introduce form components
    Created form for Personal Info
    - Added input for fullName, email, phone
    -  Defined basic styles for visual layout

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,12 @@
 import { useState } from "react";
 import "../styles/App.css";
 import CVForm from "./CVForm";
+import PersonalInfo from "./PersonalInfo";
 
 export default function App() {
   return (
     <div className="app">
+      <PersonalInfo />
       <CVForm />
     </div>
   );
---
+++ src/components/PersonalInfo.jsx
@@ -0,0 +1,37 @@
+import "../styles/PersonalInfo.css";
+
+export default function PersonalInfo({
+  fullName = "Bruce Wayne",
+  email = "yourEmail@gmail.com",
+  phoneNumber = "123-456-7890",
+}) {
+  return (
+    <form className="personal-info">
+      <div className="personal-info-header">
+        <h2>Personal Info</h2>
+        <input
+          type="text"
+          id="fullName"
+          name="fullName"
+          placeholder={fullName}
+          required
+        />
+        <input
+          type="email"
+          id="email"
+          name="email"
+          placeholder={email}
+          required
+        />
+
+        <input
+          type="tel"
+          id="phone"
+          name="phone"
+          placeholder={phoneNumber}
+          required
+        />
+      </div>
+    </form>
+  );
+}
