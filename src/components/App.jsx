384008575d6e4481532b2fa1bffcbba171c707c7
OneTooMany <santosismael.115@gmail.com>
2023-09-29 13:13:51 -0700

    Improve App element layout & create educatin form
    Included form for education info
    - Inputs for School, degree, dates, & location added.
    
    Styles added for EducationInfo
    - Similar styles from PersonalInfo used.
    
    Enclose Forms in class 'form-inputs'
    - Improves layout of page with flex.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,11 +2,16 @@ import { useState } from "react";
 import "../styles/App.css";
 import CVForm from "./CVForm";
 import PersonalInfo from "./PersonalInfo";
+import EduactionInfo from "./EducationInfo";
 
 export default function App() {
   return (
     <div className="app">
-      <PersonalInfo />
+      <div className="form-inputs">
+        <PersonalInfo />
+        <EduactionInfo />
+      </div>
+
       <CVForm />
     </div>
   );
---
+++ src/components/EducationInfo.jsx
@@ -0,0 +1,33 @@
+import "../styles/EducationInfo.css";
+
+export default function EduactionInfo() {
+  return (
+    <form className="education-info">
+      <div className="education-info-header">
+        <h2>Education Info</h2>
+      </div>
+      <input
+        type="text"
+        id="school"
+        name="school"
+        placeholder="School / university"
+        required
+      />
+      <input type="text" id="degree" name="degree" placeholder="Degree" />
+      <input
+        type="text"
+        id="date"
+        name="date"
+        placeholder="Start Date - End Date"
+        required
+      />
+      <input
+        type="text"
+        id="location"
+        name="location"
+        placeholder="Location"
+        required
+      />
+    </form>
+  );
+}
