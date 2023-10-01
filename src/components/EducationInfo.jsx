174e2731462d2be17754af21571640daaa1f9d24
OneTooMany <santosismael.115@gmail.com>
2023-09-30 18:21:53 -0700

    Refine 'education-info' layout
    Include new container 'input-container'
    - Will contain inputs for 'education-info'
    
    Add new btn to form
    - 'add-new'btn' will be used to add additional educatin sections.
    
     Add basic styles to new elemenets

--- src/components/EducationInfo.jsx
+++ src/components/EducationInfo.jsx
@@ -6,28 +6,35 @@ export default function EduactionInfo() {
       <div className="education-info-header">
         <h2>Education Info</h2>
       </div>
-      <input
-        type="text"
-        id="school"
-        name="school"
-        placeholder="School / university"
-        required
-      />
-      <input type="text" id="degree" name="degree" placeholder="Degree" />
-      <input
-        type="text"
-        id="date"
-        name="date"
-        placeholder="Start Date - End Date"
-        required
-      />
-      <input
-        type="text"
-        id="location"
-        name="location"
-        placeholder="Location"
-        required
-      />
+      <div className="input-container">
+        <input
+          type="text"
+          id="school"
+          name="school"
+          placeholder="School / university"
+          required
+        />
+        <input type="text" id="degree" name="degree" placeholder="Degree" />
+        <input
+          type="text"
+          id="date"
+          name="date"
+          placeholder="Start Date - End Date"
+          required
+        />
+        <input
+          type="text"
+          id="location"
+          name="location"
+          placeholder="Location"
+          required
+        />
+      </div>
+      <div className="education-info-add-new-btn-container">
+        <button className="add-new-btn">
+          <p>+ Add New</p>
+        </button>
+      </div>
     </form>
   );
 }
