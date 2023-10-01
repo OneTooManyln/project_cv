d779a1b1bc84fed070119b10d21a9c54e4326ab7
OneTooMany <santosismael.115@gmail.com>
2023-10-01 16:19:06 -0700

    Refine 'EducationInfo' layout & styles
    Changes to 'EducationInfo.jsx'
    - Added container for elements of 'education-info-container'.
    - Mv '..header' to outside of form.
    - New container of 'buttons-container'.
      - Houses new bts for save & cancel.
    
    Changes to 'EducationInfo.css'
    - Mv styles from 'education-info' to 'education-info-container'.
    - Use flex to center btns.
    - Define similiar styles for form btns.
    
    Changes to 'ExperienceInfo.css'
    - Mv defined styles for 'add-new-btn' to correct file.

--- src/components/EducationInfo.jsx
+++ src/components/EducationInfo.jsx
@@ -2,39 +2,49 @@ import "../styles/EducationInfo.css";
 
 export default function EduactionInfo() {
   return (
-    <form className="education-info" autocomplete="off">
-      <div className="education-info-header">
-        <h2>Education Info</h2>
+    <>
+      <div className="education-info-container">
+        <div className="education-info-header">
+          <h2>Education Info</h2>
+        </div>
+        <form className="education-info-form" autocomplete="off">
+          <div className="inputs-container">
+            <input
+              type="text"
+              id="school"
+              name="school"
+              placeholder="School / university"
+              required
+            />
+            <input type="text" id="degree" name="degree" placeholder="Degree" />
+            <input
+              type="text"
+              id="date"
+              name="date"
+              placeholder="Start Date - End Date"
+              required
+            />
+            <input
+              type="text"
+              id="location"
+              name="location"
+              placeholder="Location"
+              required
+            />
+          </div>
+          <div className="buttons-container">
+            <button className="cancel-btn">Cancel</button>
+            <button className="save-btn" type="submit">
+              Save
+            </button>
+          </div>
+        </form>
+        <div className="education-info-add-new-btn-container">
+          <button className="add-new-btn">
+            <p>+ Add New</p>
+          </button>
+        </div>
       </div>
-      <div className="input-container">
-        <input
-          type="text"
-          id="school"
-          name="school"
-          placeholder="School / university"
-          required
-        />
-        <input type="text" id="degree" name="degree" placeholder="Degree" />
-        <input
-          type="text"
-          id="date"
-          name="date"
-          placeholder="Start Date - End Date"
-          required
-        />
-        <input
-          type="text"
-          id="location"
-          name="location"
-          placeholder="Location"
-          required
-        />
-      </div>
-      <div className="education-info-add-new-btn-container">
-        <button className="add-new-btn">
-          <p>+ Add New</p>
-        </button>
-      </div>
-    </form>
+    </>
   );
 }
