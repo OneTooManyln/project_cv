8fbf54ee25f6644d03e5cb55f39e5a92cf9fe470
OneTooMany <santosismael.115@gmail.com>
2023-10-03 12:47:34 -0700

    Incorporate display form logic & refactor styles
    Changes to 'ExperienceInfo.jsx'
    
    Toggle visibilty with 'add-btn'
    - Form will only render after 'add-new-btn' is clicked.
    
    Include 'cancel-btn' to hide form
    - On click will call 'handleCancel' to hide form & show 'add-btn'.
    
    Changes to 'ExperienceInfo.css'
    
    Refactored 'ExperienceInfo' styles
    - Add existing styles to newly added container from 'experience-info'.
    - Use flex to center 'add-new..' btn.

--- src/components/ExperienceInfo.jsx
+++ src/components/ExperienceInfo.jsx
@@ -1,32 +1,74 @@
+import { useState } from "react";
 import "../styles/ExperienceInfo.css";
 
 export default function ExperienceInfo() {
+  const [isExperienceFormVisible, setIsExperienceFormVisible] = useState(false);
+  const [isAddButtonVisible, setIsAddButtonVisible] = useState(true);
+
+  const handleAddExperience = () => {
+    setIsExperienceFormVisible(true);
+    setIsAddButtonVisible(false);
+  };
+
+  const handleCancel = () => {
+    setIsExperienceFormVisible(false);
+    setIsAddButtonVisible(true);
+  };
+
   return (
-    <form className="experience-info" autocomplete="off">
-      <div className="experience-info-header">
-        <h2>Experience Info</h2>
+    <>
+      <div className="experience-info-container">
+        <div className="experience-info-header">
+          <h2>Experience Info</h2>
+        </div>
+        {isExperienceFormVisible && (
+          <form className="experience-info" autoComplete="off">
+            <div className="inputs-container">
+              <input
+                type="text"
+                id="company-name"
+                name="company-name"
+                placeholder="Company Name"
+              />
+              <input
+                type="text"
+                id="Position"
+                name="Position"
+                placeholder="Position"
+              />
+              <input
+                type="text"
+                id="date"
+                name="date"
+                placeholder="Start Date - End Date"
+                required
+              />
+              <input
+                type="text"
+                id="location"
+                name="location"
+                placeholder="Location"
+                required
+              />
+            </div>
+            <div className="buttons-container">
+              <button className="cancel-btn" onClick={handleCancel}>
+                Cancel
+              </button>
+              <button className="save-btn" type="submit">
+                Save
+              </button>
+            </div>
+          </form>
+        )}
+        {isAddButtonVisible && (
+          <div className="experience-info-add-new-btn-container">
+            <button className="add-new-btn" onClick={handleAddExperience}>
+              <p>+ Add New</p>
+            </button>
+          </div>
+        )}
       </div>
-      <input
-        type="text"
-        id="company-name"
-        name="company-name"
-        placeholder="Company Name"
-      />
-      <input type="text" id="Position" name="Position" placeholder="Position" />
-      <input
-        type="text"
-        id="date"
-        name="date"
-        placeholder="Start Date - End Date"
-        required
-      />
-      <input
-        type="text"
-        id="location"
-        name="location"
-        placeholder="Location"
-        required
-      />
-    </form>
+    </>
   );
 }
